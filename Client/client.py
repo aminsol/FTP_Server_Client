6b7580fab36e071b783f8eb2da2b2aae13233d14
Elias Perez <elias@csu.fullerton.edu>
2018-03-28 09:48:14 -0700

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 # This is client.py file
 import socket
+from ftplib import FTP
 
 # For file transfer
-FTP = 3333
+FTP= 3333
+
+
+
+
+filename = "text.html"
 # For communicating to server
 serverPort = 2222
 # For communicating to Client
@@ -12,6 +18,9 @@ clientPort = 1111
 # create a socket object
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+ftp = FTP(severrDomain)
+ftp.login(user="",passwd="")
+
 # get local machine name
 host = socket.gethostname()
 
@@ -19,10 +28,11 @@ host = socket.gethostname()
 clientSocket.connect((host, serverPort))
 
 # Our message
-data = b"Hello world"
+data = b"Hello World! This is a very long string."
 
 # Sending the message
 clientSocket.send(data)
 
 clientSocket.close()
+ftp.quit()
 print("Client connection Closed!")

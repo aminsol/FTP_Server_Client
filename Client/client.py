0885fceb8e83684bedc500fad0c3fd9447a10640
Elias Perez <elias@csu.fullerton.edu>
2018-03-30 00:28:26 -0700

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python3
 # This is client.py file
 import socket
-from ftplib import FTP
 
 # For file transfer
 FTP= 3333
-
-
-
-
-filename = "text.html"
-# For communicating to server
-serverPort = 2222
 # For communicating to Client
 clientPort = 1111
 
-# create a socket object
+# For communicating to server
+serverPort = 2222
+
+    # get local machine name
+host = socket.gethostname()
+    
+    # create a socket object
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ftp = FTP(severrDomain)
-ftp.login(user="",passwd="")
-
-# get local machine name
-host = socket.gethostname()
-
-# connection to hostname on the port.
+    # connection to hostname on the port.
 clientSocket.connect((host, serverPort))
 
-# Our message
-data = b"Hello World! This is a very long string."
-
-# Sending the message
-clientSocket.send(data)
-
+filename = input("Enter Filename: ")
+if filename != 'q':
+    
+        # Sending the message
+    clientSocket.send(filename)
+    data = s.recv(1024)
+    if data[:6] == 'EXISTS':
+        filesize = long(data[6:])
+        message = input("File Exists, "+(filesize)+\
+                                "Bytes, download? (Y/N): ")
+        if message == 'Y':
+            clientSocket.send('OK')
+            f = open('new_'+filename, 'wb')
+            data = clientSocket.recv(1024)
+            totalRecv = len(data)
+            f.write(data)
+            while totalRecv < filesize:
+                data = s.recv(1024)
+                totalRecv = len(data)
+                f.write(data)
+                print ("{0:.2f}",format((totalRecv/float(filesize))*100)+\
+                    "% Done")
+            print ("Downlad Complete!")
+    else:
+        print ("File does not exist")
+                
 clientSocket.close()
-ftp.quit()
-print("Client connection Closed!")
+print   ("Client connection Closed!")

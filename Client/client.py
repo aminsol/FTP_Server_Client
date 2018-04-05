4454200cd9cde2b52c5d7916e96f7082ea373849
Levoo <fzavala649@gmail.com>
2018-04-05 03:27:12 -0700

    Sends data but is wrong wip

--- Client/client.py
+++ Client/client.py
@@ -1,28 +1,81 @@
 #!/usr/bin/python3
 # This is client.py file
-import socket
+import socket, sys, os
 
-# For file transfer
-FTP = 3333
-# For communicating to server
-serverPort = 2222
-# For communicating to Client
-clientPort = 1111
+def getSize(file):
+    fs = os.stat(file)
+    return fs.st_size
 
-# create a socket object
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def uploadFile(clientSocket, commPort, uInput):
+    try:
+        fObj = open(uInput, "r")
+        fileSize = getSize(uInput)
+        bytesSent = 0 # keep track of bytes sent
+        ack = 0  # acknolage that the bytes were recievced
+        #clientSocket.send(b"u") # send upload command to server
+        fileData = None
+        while True:
+            fileData = fObj.read(65536)
+            if fileData:
+                dataSizeStr = str(len(fileData))
+                while len(dataSizeStr) < 10:
+                    dataSizeStr = "0" + dataSizeStr
 
-# get local machine name
-host = socket.gethostname()
+                fileData += dataSizeStr
+                while len(fileData) > bytesSent:
+                    bytesSent += clientSocket.send(bytearray(fileData[bytesSent:], 'utf-8'))
+            else:
+                break
+        fObj.close()
+    except FileNotFoundError:
+        print("File not found...")
 
-# connection to hostname on the port.
-clientSocket.connect((host, serverPort))
+def main():
+    try: #check that valid port number has been passed
+        serverMachine = str(sys.argv[1])
+        serverPort  = int(sys.argv[2]) # attempt to assign port number to FTP
+    except IndexError:
+        print("Invalid server name / port number: usage 'python3 client.py <server machine> <port number>'\n")
+        sys.exit()
+    except ValueError: # if something other than int was sent we get value error
+        print("Invalid server name / port number: usage 'python3 client.py <server machine> <port number>\n")
+        sys.exit()
 
-# Our message
-data = b"Hello world"
+    # For data transfer to/from CLient/server
+    FTP = 2222
+    # For communicating to/from Client/Server
+    commPort = 1111
 
-# Sending the message
-clientSocket.send(data)
+    # create a socket object
+    # get local machine name
+    # host = socket.gethostname()
 
-clientSocket.close()
-print("Client connection Closed!")
+    # connection to hostname on the port. to test use loopback adress 127.0.0.1
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object
+        clientSocket.connect((serverMachine, serverPort))  # connection to hostname on the port. to test use loopback adress 127.0.0.1
+    except ConnectionError:
+        print("Error connecting to " + str(serverMachine) + "\n")
+    except socket.gaierror:
+        print("Unknown name or service")
+
+    uInput = [""] # user input and while loop 
+    print("Connection to server successful")
+    while (uInput[0] != "quit"):
+        uInput = input("ftp> ").split(" ")
+        try:
+            if uInput[0] == "get": # Elias
+                pass
+            elif uInput[0] == "put": # fernando
+                uploadFile(clientSocket, commPort, uInput[1]) # should have file name in uInput[1]
+            elif uInput[0] == "ls": # Karla
+                pass
+        except IndexError:
+            print("No filename provided: get <filename> / put <filename>")
+
+
+    clientSocket.close()
+    print("Client connection Closed!")
+
+
+main()
--- Server/server.py
+++ Server/server.py
@@ -1,36 +1,72 @@
 #!/usr/bin/python3
 # This is server.py file
+#fernando's part
 import socket
+import sys, os
 
-# The ports on which to listen
 
-# For file transfer
-FTP = 3333
-# For communicating to server
-serverPort = 2222
-# For communicating to Client
-clientPort = 1111
+def recvData(sock, nBytes):
+    rBuffer = ""
+    tmpBuffer = ""
+    while len(rBuffer) < nBytes:
+        tmpBuffer = sock.recv(nBytes)
 
-# Create a TCP socket
-serverSocket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-serverSocket.bind(('',serverPort))
+        if not tmpBuffer:
+            break
+        rBuffer += str(tmpBuffer)
+    
+    return rBuffer
 
-# Start listening for incoming connections
-serverSocket.listen(serverPort)
-
-print("The server is ready to receive")
-
-# The buffer to storetherreceived data
-data=""
 
 # Forever accept incoming connections
-while True:
-    # Accept a connection; get client’s socket
-    connectionSocket,addr=serverSocket.accept()
-    # Receive whatever the newly connected client has to send
-    data = connectionSocket.recv(40)
-    print(data)
+def s_main():
+    try: #check that valid port number has been passed
+        listenPort  = int(sys.argv[1]) # attempt to assign port number to FTP
+    except IndexError: # if no argument was sent then we get index error
+        print("Invalid port number: usage 'python3 server.py <port number>'\n")
+        sys.exit()
+    except ValueError: # if something other than int was sent we get value error
+        print("Invalid port number: usage 'python3 server.py <port number>'\n")
+        sys.exit()
+    
+    # For data transfer to/from CLient/server
+    FTP = 2222
+    # For communicating to/from Client/Server
+    commPort = 1111
 
-    # Close the socket
-    connectionSocket.close()
+    # Create a TCP socket
+    serverSocket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    serverSocket.bind(('',listenPort))
+
+    # Start listening for incoming connections
+    serverSocket.listen(commPort)
+
+    print("The server is ready to receive")
+
+    # The buffer to storetherreceived data
+    data=""
+
+    while True:
+        # Accept a connection; get client’s socket
+        connectionSocket,addr=serverSocket.accept()
+        # Receive whatever the newly connected client has to send
+        print("Connection from client: " + str(addr) + "\n")
+
+        fileData = ""
+        rBuffer = ""
+        fSize = 0
+        fsBuffer = ""
+
+        fsBuffer = recvData(connectionSocket, 10)
+        fSize = len(fsBuffer)
+
+        fileData = recvData(connectionSocket, fSize)
+
+        print(fileData)
+
+        # Close the socket
+        connectionSocket.close()
+
+
+s_main()

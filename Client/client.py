30cd95e7f59b31a2fb95187adca3221b460ce49b
Levoo <fzavala649@gmail.com>
2018-04-05 13:56:52 -0700

    file upload finished

--- Client/client.py
+++ Client/client.py
@@ -2,38 +2,49 @@
 # This is client.py file
 import socket, sys, os
 
+# For data transfer to/from CLient/server
+FTP = 2222
+hostt = "127.0.0.1"
 def getSize(file):
-    fs = os.stat(file)
-    return fs.st_size
+    fs = os.stat(file) # asume file is in same dir as the client file 
+    return fs.st_size  # return size 
 
-def uploadFile(clientSocket, commPort, uInput):
+def uploadFile(clientCommSocket, host, uInput): # pass communication socket hostname and file name
     try:
-        fObj = open(uInput, "r")
-        fileSize = getSize(uInput)
-        bytesSent = 0 # keep track of bytes sent
-        ack = 0  # acknolage that the bytes were recievced
-        #clientSocket.send(b"u") # send upload command to server
-        fileData = None
-        while True:
-            fileData = fObj.read(65536)
-            if fileData:
-                dataSizeStr = str(len(fileData))
-                while len(dataSizeStr) < 10:
-                    dataSizeStr = "0" + dataSizeStr
+        fObj = open(uInput, "rb")
+        fileSize = str(getSize(uInput)).encode('utf-8')
 
-                fileData += dataSizeStr
-                while len(fileData) > bytesSent:
-                    bytesSent += clientSocket.send(bytearray(fileData[bytesSent:], 'utf-8'))
-            else:
-                break
+        clientCommSocket.send(uInput.encode('utf-8'))
+        ackMsg = clientCommSocket.recv(1024).decode("utf-8").strip()
+        while ackMsg != "r":
+            clientCommSocket.send(uInput.encode('utf-8'))
+        
+        clientCommSocket.send(fileSize)
+        ackMsg = clientCommSocket.recv(1024).decode("utf-8").strip()
+        while ackMsg != "r":
+            clientCommSocket.send(fileSize)
+        print ("Sent filename: " + uInput + " size " + str(getSize(uInput)) + "\n")
+
+        clientCtrSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientCtrSocket.connect((host, FTP))
+        print("FTP connection established commncing upload")
+        bytesSent = 0
+        while bytesSent < int(fileSize):
+            fileData = fObj.read()
+            if fileData:
+                bytesSent += clientCtrSocket.send(fileData)
+            print("Sent: " + str(bytesSent) + " / " + str(fileSize).decode() )
+        print("File upload done...\n")
         fObj.close()
     except FileNotFoundError:
         print("File not found...")
+    except ConnectionError:
+        print("Error connecting to FTP port \n")
 
-def main():
+def main(): # this is for me the upload file function is the important part 
     try: #check that valid port number has been passed
         serverMachine = str(sys.argv[1])
-        serverPort  = int(sys.argv[2]) # attempt to assign port number to FTP
+        commPort  = int(sys.argv[2]) # attempt to assign port number to FTP
     except IndexError:
         print("Invalid server name / port number: usage 'python3 client.py <server machine> <port number>'\n")
         sys.exit()
@@ -41,19 +52,10 @@ def main():
         print("Invalid server name / port number: usage 'python3 client.py <server machine> <port number>\n")
         sys.exit()
 
-    # For data transfer to/from CLient/server
-    FTP = 2222
-    # For communicating to/from Client/Server
-    commPort = 1111
-
-    # create a socket object
-    # get local machine name
-    # host = socket.gethostname()
-
     # connection to hostname on the port. to test use loopback adress 127.0.0.1
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object
-        clientSocket.connect((serverMachine, serverPort))  # connection to hostname on the port. to test use loopback adress 127.0.0.1
+        clientSocket.connect((serverMachine, commPort))  # connection to hostname on the port. to test use loopback adress 127.0.0.1
     except ConnectionError:
         print("Error connecting to " + str(serverMachine) + "\n")
     except socket.gaierror:
@@ -67,7 +69,7 @@ def main():
             if uInput[0] == "get": # Elias
                 pass
             elif uInput[0] == "put": # fernando
-                uploadFile(clientSocket, commPort, uInput[1]) # should have file name in uInput[1]
+                uploadFile(clientSocket, serverMachine, uInput[1]) # should have file name in uInput[1]
             elif uInput[0] == "ls": # Karla
                 pass
         except IndexError:
--- Server/server.py
+++ Server/server.py
@@ -2,71 +2,70 @@
 # This is server.py file
 #fernando's part
 import socket
-import sys, os
+import sys, os, time
 
+# For data transfer to/from CLient/server
+FTP = 2222
 
-def recvData(sock, nBytes):
-    rBuffer = ""
-    tmpBuffer = ""
-    while len(rBuffer) < nBytes:
-        tmpBuffer = sock.recv(nBytes)
+def retrieveUpload (connSocket, requester): # pass it the connection socket and person requesting
+    filename = connSocket.recv(1024).decode("utf-8").strip() # get name and remove any garbage 
+    connSocket.send(b"r")
+    fileSize = connSocket.recv(1024).decode("utf-8").strip()
+    connSocket.send(b"r")
+    print ("Recieved upload request for file: " + filename + " with size: " + fileSize + " bytes\n")
+    print ("Commencing upload request from client:" + requester + " for " + filename + "\n")
 
-        if not tmpBuffer:
-            break
-        rBuffer += str(tmpBuffer)
+    serverCtrSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverCtrSocket.bind(('', FTP))
+    serverCtrSocket.listen(20)
+    print("Established FTP port waiting for client connection...")
+    ctrlSock, add = serverCtrSocket.accept()
+
+    print("FTP connection established commencing client file upload...\n")
+    bytesRcv = 0
+    filedata = ""
+    fObj = open(filename, "w")
+    while bytesRcv < int(fileSize):
+        filedata += ctrlSock.recv(1024).decode()
+        bytesRcv += len(filedata)
+        print("Rcv'ed: " + str(bytesRcv) + " / " + fileSize )
     
-    return rBuffer
+    fObj.write(filedata)
+    print ("File recived and stored")
+    ctrlSock.close()
 
 
 # Forever accept incoming connections
-def s_main():
+def s_main(): # again just for me, retrieveUpload is the important file
     try: #check that valid port number has been passed
-        listenPort  = int(sys.argv[1]) # attempt to assign port number to FTP
+        commPort  = int(sys.argv[1]) # attempt to assign port number to FTP
     except IndexError: # if no argument was sent then we get index error
         print("Invalid port number: usage 'python3 server.py <port number>'\n")
         sys.exit()
     except ValueError: # if something other than int was sent we get value error
         print("Invalid port number: usage 'python3 server.py <port number>'\n")
         sys.exit()
-    
-    # For data transfer to/from CLient/server
-    FTP = 2222
-    # For communicating to/from Client/Server
-    commPort = 1111
+
 
     # Create a TCP socket
     serverSocket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Bind the socket to the port
-    serverSocket.bind(('',listenPort))
+    serverSocket.bind(('',commPort))
 
     # Start listening for incoming connections
-    serverSocket.listen(commPort)
+    serverSocket.listen(20) # 20 conncurent listens 
+    # not in loop for easier testing 
+    print("The server is ready to receive communication on port " + str(commPort) + "\n")
 
-    print("The server is ready to receive")
+    # Accept a connection; get client’s socket
+    connectionSocket,addr=serverSocket.accept()
+    # Receive whatever the newly connected client has to send
+    print("Connection from client: " + str(addr) + "\n")
 
-    # The buffer to storetherreceived data
-    data=""
-
-    while True:
-        # Accept a connection; get client’s socket
-        connectionSocket,addr=serverSocket.accept()
-        # Receive whatever the newly connected client has to send
-        print("Connection from client: " + str(addr) + "\n")
-
-        fileData = ""
-        rBuffer = ""
-        fSize = 0
-        fsBuffer = ""
-
-        fsBuffer = recvData(connectionSocket, 10)
-        fSize = len(fsBuffer)
-
-        fileData = recvData(connectionSocket, fSize)
-
-        print(fileData)
-
-        # Close the socket
-        connectionSocket.close()
+    retrieveUpload(connectionSocket, str(addr)) # attempt to get upload
+    
+    # Close the socket
+    connectionSocket.close()
 
 
 s_main()

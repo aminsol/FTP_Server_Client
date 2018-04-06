65f380aaeafded0c15282bfc55841d768f66ae88
Elias Perez <elias@csu.fullerton.edu>
2018-04-06 11:24:47 -0700

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -111,33 +111,32 @@ command = ""
 
 while command != "exit":
     command = input("Enter your command: ")
-    if command != "exit":
-        result = ""
-        if lscommand.match(command):
-            print(send(command))
-        elif downloadFile.match(command):
-            fileName = downloadFile.match(command)[1]
-            # Asking server to send us a file
-            # if the file exist then server response with file size
-            size = send("download " + fileName)
-            if size != "err":
-                print("Start downloading..")
-                result = download(fileName, size)
-                print("Download is finished!")
-            else:
-                # cannot find the file on the server
-                print("No such file found on the server!")
-        elif uploadFile.match(command):
-            fileName = uploadFile.match(command)[1]
-            fileSize = getSize(fileName)
-            if fileSize:
-                if send("upload " + fileName + " " + str(fileSize)) == "ok":
-                    result = upload(fileName)
+    result = ""
+    if lscommand.match(command):
+        print(send(command))
+    elif downloadFile.match(command):
+        fileName = downloadFile.match(command)[1]
+        # Asking server to send us a file
+        # if the file exist then server response with file size
+        size = send("download " + fileName)
+        if size != "err":
+            print("Start downloading..")
+            result = download(fileName, size)
+            print("Download is finished!")
         else:
-            print("Please use one of the following command:")
-            print("upload <File Name>")
-            print("download <File Name>")
-            print("ls")
-            print("exit")
+            # cannot find the file on the server
+            print("No such file found on the server!")
+    elif uploadFile.match(command):
+        fileName = uploadFile.match(command)[1]
+        fileSize = getSize(fileName)
+        if fileSize:
+            if send("upload " + fileName + " " + str(fileSize)) == "ok":
+                result = upload(fileName)
+    elif command !="exit":
+        print("Please use one of the following command:")
+        print("upload <File Name>")
+        print("download <File Name>")
+        print("ls")
+        print("exit")
 
 print("Client connection Closed!")
